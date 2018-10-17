aef146aa90c9254d7498203c53ff1a0688d9bd7e
ricardogwill <ricardogwill@gmail.com>
2018-10-16 22:53:54 -0400

    Initial commit.

---
+++ app/src/main/java/com/ricardogwill/bigdecimalcalculatorapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ricardogwill.bigdecimalcalculatorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView inputTextView, outputTextView;
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputTextView = findViewById(R.id.input_textView);
+        outputTextView = findViewById(R.id.output_textView);
+        submitButton = findViewById(R.id.submit_button);
+
+    }
+}
